a047b7092f7d5c17e1ac18d4bbe7f9c5e8b7ee33
John Eckert <johnteckert@gmail.com>
2023-07-26 16:59:23 -0400

    setup linting better

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,23 +4,26 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
     'plugin:react/jsx-runtime',
+    'eslint-config-prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
   },
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
     project: ['./tsconfig.json', './tsconfig.node.json'],
     tsconfigRootDir: __dirname,
-   },
-}
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
