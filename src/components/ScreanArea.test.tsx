db1f3157bb36f936d16a8ea8fd38837a6328545f
John Eckert <johnteckert@gmail.com>
2023-07-28 21:06:12 -0400

    Add unit tests for ScreenArea

--- src/components/ScreanArea.test.tsx
+++ src/components/ScreanArea.test.tsx
@@ -1,15 +1,34 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import ScreenArea from './ScreenArea';
 
 jest.mock('react-dnd', () => ({
   useDrop: () => [[], jest.fn()],
 }));
 
-jest.mock('./DraggableCard.tsx', () => () => <div />);
+jest.mock('./DraggableCard.tsx', () => () => <div data-testid="draggable-card" />);
 
 describe('ScreenArea', () => {
   it('renders', () => {
     render(<ScreenArea />);
     expect(screen.getByTestId('screen-area')).toBeInTheDocument();
+  });
+
+  it('renders the correct number of draggable cards', () => {
+    render(<ScreenArea />);
+    expect(screen.getAllByTestId('draggable-card')).toHaveLength(2);
+  });
+
+  it('resizes the screen area when the window is resized', async () => {
+    render(<ScreenArea />);
+    const screenArea = screen.getByTestId('screen-area');
+    const resizeEvent = new Event('resize');
+    window.innerWidth = 1000;
+    window.innerHeight = 1000;
+    window.dispatchEvent(resizeEvent);
+
+    await waitFor(() => {
+      expect(screenArea).toHaveStyle('width: 1000px');
+      expect(screenArea).toHaveStyle('height: 1000px');
+    });
   });
 });
