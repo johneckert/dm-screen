85b65ba9293f1539939b257df9873209d7f8f380
John Eckert <johnteckert@gmail.com>
2023-08-18 15:35:14 -0400

    some styling

--- src/components/layout/ExpandedCard.tsx
+++ src/components/layout/ExpandedCard.tsx
@@ -5,11 +5,16 @@ import Box from '@mui/material/Box';
 import { makeStyles } from '@mui/styles';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
 import { Theme } from '@mui/material/styles';
-import { update } from 'lodash';
+import EditIcon from '@mui/icons-material/Edit';
+import CheckIcon from '@mui/icons-material/Check';
 
-const useStyles = makeStyles<Theme>((theme) => ({
+interface StyleProps {
+  isEditing: boolean;
+}
+
+export const useStyles = makeStyles<Theme, StyleProps>((theme) => ({
   modal: {
     position: 'absolute',
     top: '50%',
@@ -18,16 +23,48 @@ const useStyles = makeStyles<Theme>((theme) => ({
     width: '50%',
     height: '80%',
     backgroundColor: '#ffffff',
-    border: '2px solid #000',
+    border: 'none',
+    borderRadius: theme.spacing(1.5),
     boxShadow: '24px',
-    padding: '4px',
+    padding: theme.spacing(2),
     display: 'flex',
     flexDirection: 'column',
   },
-  editButton: {
-    alignSelf: 'flex-end',
+  header: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: theme.spacing(2),
+    marginBottom: theme.spacing(2),
+  },
+  body: {
+    display: 'flex',
+    flexDirection: 'column',
+    padding: theme.spacing(2),
+  },
+  modalTitle: {
     margin: theme.spacing(4),
+    paddingX: theme.spacing(2),
+    paddingTop: theme.spacing(1.5),
+  },
+  titleInput: {
+    '& input': {
+      fontSize: theme.spacing(6),
+      fontWeight: 400,
+    },
+  },
+  modalContent: {
+    margin: theme.spacing(4),
+    paddingX: theme.spacing(2),
+    paddingTop: theme.spacing(3),
+  },
+  editButton: {
+    alignSelf: 'center',
+    justifyContent: 'center',
     width: 'fit-content',
+    padding: theme.spacing(1),
+    marginLeft: 'auto',
   },
 }));
 
@@ -38,10 +75,10 @@ interface ExpandedCardProps {
 }
 
 const ExpandedCard: React.FC<ExpandedCardProps> = ({ closeExpandedCard, expandedCardData, updateCard }) => {
-  const classes = useStyles();
   const [isEditing, setIsEditing] = useState(false);
   const [title, setTitle] = useState(expandedCardData.title);
   const [content, setContent] = useState(expandedCardData.content);
+  const classes = useStyles({ isEditing });
   const handleClose = () => {
     closeExpandedCard();
   };
@@ -63,35 +100,47 @@ const ExpandedCard: React.FC<ExpandedCardProps> = ({ closeExpandedCard, expanded
       <Box className={classes.modal}>
         {isEditing ? (
           <>
-            <TextField
-              id="modal-title"
-              label="title"
-              variant="outlined"
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
-            />
-            <TextField
-              id="modal-content"
-              label="content"
-              multiline
-              rows={4}
-              value={content}
-              onChange={(e) => setContent(e.target.value)}
-            />
+            <Box className={classes.header}>
+              <TextField
+                id="modal-title"
+                className={classes.titleInput}
+                fullWidth
+                variant="standard"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+              />
+              <IconButton className={classes.editButton} aria-label="edit-save-button" onClick={handleEdit}>
+                <CheckIcon />
+              </IconButton>
+            </Box>
+            <Box className={classes.body}>
+              <TextField
+                id="modal-content"
+                fullWidth
+                multiline
+                rows={18}
+                value={content}
+                onChange={(e) => setContent(e.target.value)}
+              />
+            </Box>
           </>
         ) : (
           <>
-            <Typography id="modal-title" variant="h6" component="h2">
-              {title}
-            </Typography>
-            <Typography id="modal-content" sx={{ mt: 2 }}>
-              {content}
-            </Typography>
+            <Box className={classes.header}>
+              <Typography id="modal-title" className={classes.modalTitle} variant="h3" component="h3">
+                {title}
+              </Typography>
+              <IconButton className={classes.editButton} aria-label="edit-save-button" onClick={handleEdit}>
+                <EditIcon />
+              </IconButton>
+            </Box>
+            <Box className={classes.body}>
+              <Typography id="modal-content" className={classes.modalContent}>
+                {content}
+              </Typography>
+            </Box>
           </>
         )}
-        <Button variant="contained" className={classes.editButton} onClick={handleEdit}>
-          {isEditing ? 'Save' : 'Edit'}
-        </Button>
       </Box>
     </Modal>
   );
