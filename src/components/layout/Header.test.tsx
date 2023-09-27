92c5a064c4d52765337ae0582a13438cbda37783
John Eckert <johnteckert@gmail.com>
2023-09-26 20:34:30 -0400

    update unit tests

--- src/components/layout/Header.test.tsx
+++ src/components/layout/Header.test.tsx
@@ -1,9 +1,28 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, act } from '@testing-library/react';
 import Header from './Header';
 
 describe('ScreenArea', () => {
   it('renders', () => {
     render(<Header />);
     expect(screen.getByTestId('header')).toBeInTheDocument();
+  });
+
+  it('renders title', () => {
+    render(<Header />);
+    expect(screen.getByText(/dm screen/i)).toBeInTheDocument();
+  });
+
+  it('renders download button', () => {
+    render(<Header />);
+    expect(screen.getByRole('button', { name: /download/i })).toBeInTheDocument();
+  });
+
+  it('downloads cards when download button is clicked', () => {
+    jest.spyOn(document, 'createElement');
+
+    render(<Header />);
+
+    screen.getByRole('button', { name: /download/i }).click();
+    expect(document.createElement).toHaveBeenCalledWith('a');
   });
 });
