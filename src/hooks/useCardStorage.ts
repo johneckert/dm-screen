b809eaab09eaf96e61231198a922b236858ca743
John Eckert <johnteckert@gmail.com>
2023-10-03 16:21:10 -0400

    refactor

--- src/hooks/useCardStorage.ts
+++ src/hooks/useCardStorage.ts
@@ -4,20 +4,18 @@ import { WELCOME_CARDS } from '../welcomeCard';
 
 const useCardStorage: () => [CardDataMap, (cards: CardDataMap) => void] = () => {
   const [storedCards, setStoredCards] = useLocalStorage('cards', WELCOME_CARDS);
+  const emptyCardMap = { 'droppable-1': [], 'droppable-2': [], 'droppable-3': [], 'droppable-4': [] } as CardDataMap;
 
-  const filterCards = () => {
-    const filteredCards: CardDataMap = { 'droppable-1': [], 'droppable-2': [], 'droppable-3': [], 'droppable-4': [] };
-    storedCards.forEach((card) => {
-      filteredCards[card.column].push(card);
-    });
+  const mappedCards = storedCards.reduce((filteredCards: CardDataMap, card) => {
+    filteredCards[card.column].push(card);
     return filteredCards;
-  };
+  }, emptyCardMap);
 
   const setCards = (cards: CardDataMap) => {
     setStoredCards(Object.values(cards).flat());
   };
 
-  return [filterCards(), setCards];
+  return [mappedCards, setCards];
 };
 
 export default useCardStorage;
