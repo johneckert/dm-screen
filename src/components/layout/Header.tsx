e8535a1d215eb2e4cafebbbfef6752317da70bf4
John Eckert <johnteckert@gmail.com>
2023-10-03 22:20:27 -0400

    basic upload

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,8 +1,24 @@
+import React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import SaveAltIcon from '@mui/icons-material/SaveAlt';
+import Button from '@mui/material/Button';
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import { styled } from '@mui/material/styles';
+
+const VisuallyHiddenInput = styled('input')({
+  clip: 'rect(0 0 0 0)',
+  clipPath: 'inset(50%)',
+  height: 1,
+  overflow: 'hidden',
+  position: 'absolute',
+  bottom: 0,
+  left: 0,
+  whiteSpace: 'nowrap',
+  width: 1,
+});
 
 const Header = () => {
   const downloadCards = () => {
@@ -15,6 +31,19 @@ const Header = () => {
     link.click();
     link.remove();
   };
+
+  const uploadCards = (e: React.SyntheticEvent) => {
+    const target = e.target as HTMLInputElement;
+    const file = target.files?.[0];
+    const reader = new FileReader();
+    reader.onload = (event) => {
+      const cards = JSON.parse(event.target?.result as string);
+      localStorage.setItem('cards', cards);
+    };
+    reader.readAsText(file as Blob);
+    window.location.reload();
+  };
+
   return (
     <AppBar position="static" data-testid="header">
       <Toolbar>
@@ -32,6 +61,10 @@ const Header = () => {
         >
           <SaveAltIcon />
         </IconButton>
+        <Button component="label" variant="contained" startIcon={<CloudUploadIcon />}>
+          Upload file
+          <VisuallyHiddenInput type="file" onChange={uploadCards} />
+        </Button>
       </Toolbar>
     </AppBar>
   );
