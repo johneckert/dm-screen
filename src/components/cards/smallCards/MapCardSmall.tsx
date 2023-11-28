be927ce25f2dc5f92742c0b016e9f20c9c9f3929
John Eckert <johnteckert@gmail.com>
2023-11-28 15:14:59 -0500

    fix no divs as children of p

--- src/components/cards/smallCards/MapCardSmall.tsx
+++ src/components/cards/smallCards/MapCardSmall.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import { MapCardContent } from '../../../interfaces';
 import ReactMarkdown from 'react-markdown';
-import { Box, Typography } from '@mui/material';
+import { Box } from '@mui/material';
 import { PURPLE } from '../../../colors';
 
 const MapCardSmall: React.FC<{ content: MapCardContent }> = ({ content }) => {
   const { readOutLoudText = '' } = content ?? '';
   return (
-    <Box sx={{ bgcolor: PURPLE[200], py: 2, px: 1, borderRadius: 2, height: '100%' }} data-testid="small-room-card">
-      <Typography sx={{ fontSize: 14, maxHeight: '100%', overflow: 'hidden' }}>
-        <ReactMarkdown>
-          {readOutLoudText.length > 200 ? `${readOutLoudText.substring(0, 200)}...` : readOutLoudText}
-        </ReactMarkdown>
-      </Typography>
+    <Box
+      sx={{ bgcolor: PURPLE[200], py: 2, px: 1, borderRadius: 2, height: '100%', overflow: 'hidden' }}
+      data-testid="small-room-card"
+    >
+      <ReactMarkdown>
+        {readOutLoudText.length > 200 ? `${readOutLoudText.substring(0, 200)}...` : readOutLoudText}
+      </ReactMarkdown>
     </Box>
   );
 };
--- src/components/cards/smallCards/NoteCardSmall.tsx
+++ src/components/cards/smallCards/NoteCardSmall.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import { NoteCardContent } from '../../../interfaces';
-import { Box, Typography } from '@mui/material';
+import { Box } from '@mui/material';
 import ReactMarkdown from 'react-markdown';
 
 const NoteCardSmall: React.FC<{ content: NoteCardContent }> = ({ content }) => {
   const { notes = '' } = content ?? '';
   return (
-    <Box sx={{ px: 1, height: '100%' }} data-testid="small-note-card">
-      <Typography sx={{ fontSize: 14, maxHeight: '100%', overflow: 'hidden' }}>
-        <ReactMarkdown>{notes.length > 300 ? `${notes.substring(0, 300)}...` : notes}</ReactMarkdown>
-      </Typography>
+    <Box sx={{ px: 1, height: '100%', overflow: 'hidden' }} data-testid="small-note-card">
+      <ReactMarkdown>{notes.length > 300 ? `${notes.substring(0, 300)}...` : notes}</ReactMarkdown>
     </Box>
   );
 };
