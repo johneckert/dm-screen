4ea576bac437c3caf2eadf171e74cb6a52bef6a9
John Eckert <johnteckert@gmail.com>
2023-12-05 16:56:37 -0500

    create with custom name and delete

--- src/App.tsx
+++ src/App.tsx
@@ -10,11 +10,13 @@ import Header from './components/header/Header';
 import ScreenArea from './components/layout/ScreenArea';
 import ErrorBoundary from './ErrorBoundry';
 import { Tabs, Tab } from '@mui/material';
-import { generateSlug, RandomWordOptions } from 'random-word-slugs';
+import NewTabDialog from './components/modals/NewTabDialog';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 function App() {
   const [isSticky, setSticky] = useState<boolean>(false);
   const [tabs, setTabs] = useTabStorage();
+  const [showNewTabDialog, setshowNewTabDialog] = useState<boolean>(false);
   const [activeTab, setActiveTab] = useActiveTabStorage();
   const [showNewCardModal, setShowNewCardModal] = useState<boolean>(false);
   const stickyStyles = {
@@ -24,29 +26,28 @@ function App() {
     width: '100%',
     zIndex: 999,
   };
-  const createNewTab = () => {
-    const options: RandomWordOptions<2> = {
-      format: 'kebab',
-      partsOfSpeech: ['adjective', 'noun'],
-      categories: {
-        adjective: ['personality'],
-        noun: ['animals'],
-      },
-    };
-    const tabName = generateSlug(2, options);
-    return tabName;
+  const createNewTab = (name: string) => {
+    setTabs([...tabs, name]);
+    setActiveTab(name);
   };
 
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore-next-line
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const handleTabChange = (_, newValue: string) => {
-    let nextTab = newValue;
+    console.log(newValue);
     if (newValue === 'newTab') {
-      nextTab = createNewTab();
-      setTabs([...tabs, nextTab]);
+      setshowNewTabDialog(true);
+      return;
     }
-    setActiveTab(nextTab);
+    if (newValue === 'deleteTab') {
+      console.log('delete tab');
+      const remainingTabs = tabs.filter((tab) => tab !== activeTab);
+      setTabs(remainingTabs);
+      setActiveTab(remainingTabs[0]);
+      return;
+    }
+    setActiveTab(newValue);
   };
 
   useEffect(() => {
@@ -71,12 +72,25 @@ function App() {
         setActiveTab={setActiveTab}
         setShowNewCardModal={setShowNewCardModal}
       />
-      <Tabs value={activeTab} onChange={handleTabChange} sx={isSticky ? stickyStyles : {}}>
+      <Tabs
+        variant="scrollable"
+        scrollButtons="auto"
+        value={activeTab}
+        onChange={handleTabChange}
+        sx={isSticky ? stickyStyles : {}}
+      >
         {tabs.map((tab: string) => (
           <Tab key={tab} label={tab} value={tab} />
         ))}
         <Tab key="newTab" label="+ Tab" value="newTab" />
+        <Tab key="deleteTab" icon={<DeleteIcon />} value="deleteTab" />
       </Tabs>
+      <NewTabDialog
+        showNewTabDialog={showNewTabDialog}
+        setshowNewTabDialog={setshowNewTabDialog}
+        createNewTab={createNewTab}
+        tabs={tabs}
+      />
       <ErrorBoundary>
         <ScreenArea
           activeTab={activeTab}
---
+++ src/components/modals/NewTabDialog.tsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import { Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions, Button, TextField } from '@mui/material';
+
+interface NewTabDialogProps {
+  showNewTabDialog: boolean;
+  setshowNewTabDialog: (show: boolean) => void;
+  createNewTab: (name: string) => void;
+  tabs: string[];
+}
+
+const NewTabDialog: React.FC<NewTabDialogProps> = ({ showNewTabDialog, setshowNewTabDialog, createNewTab, tabs }) => {
+  const [name, setName] = useState<string>('');
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => setName(e.target.value);
+  const handleClose = () => {
+    setName('');
+    setshowNewTabDialog(false);
+  };
+  const handleCreate = () => {
+    createNewTab(name);
+    handleClose();
+  };
+
+  return (
+    <Dialog open={showNewTabDialog} onClose={handleClose}>
+      <DialogTitle>Create Tab</DialogTitle>
+      <DialogContent>
+        <DialogContentText>Please enter a name for your new tab.</DialogContentText>
+        <TextField
+          autoFocus
+          margin="dense"
+          id="tab-name"
+          label="Tab Name"
+          type="email"
+          fullWidth
+          variant="standard"
+          onChange={handleInputChange}
+          value={name}
+        />
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose}>Cancel</Button>
+        <Button disabled={tabs.includes(name) || name === ''} onClick={handleCreate}>
+          Create New Tab
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+export default NewTabDialog;
