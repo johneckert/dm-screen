566bfaa890c73fcb6692bdcf051f7198dddb4fd7
John Eckert <johnteckert@gmail.com>
2023-12-07 14:20:19 -0500

    add default rules tab

--- src/welcomeCard.ts
+++ src/welcomeCard.ts
@@ -1,5 +1,16 @@
 import { CardData, CardType } from './interfaces';
 import { DEFAULT_TABS } from './constants';
+import { RULES } from './ruleData';
+
+const RULE_CARDS: CardData[] = RULES.map((rule: string, index) => ({
+  id: `rule-${rule}`,
+  content: {
+    title: rule,
+  },
+  column: `droppable-${(index % 4) + 1}`,
+  tab: DEFAULT_TABS[1],
+  type: CardType.Rule,
+}));
 
 export const WELCOME_CARDS: CardData[] = [
   {
@@ -9,7 +20,8 @@ export const WELCOME_CARDS: CardData[] = [
       notes: 'This is a digital DM screen for D&D 5e. It is designed to be used while DM-ing games online.',
     },
     column: 'droppable-1',
-    tab: DEFAULT_TABS,
+    tab: DEFAULT_TABS[0],
     type: CardType.Note,
   },
+  ...RULE_CARDS,
 ];
