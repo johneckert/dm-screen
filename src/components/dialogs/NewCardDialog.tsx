a60c046ee586e171d6042d6bb3670d4a22b2cac1
John Eckert <johnteckert@gmail.com>
2023-12-15 12:07:10 -0500

    refactor dialogs

--- src/components/dialogs/NewCardDialog.tsx
+++ src/components/dialogs/NewCardDialog.tsx
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
 import { useReadLocalStorage } from 'usehooks-ts';
 import { Modal, Box, Typography, Button } from '@mui/material';
-import { makeStyles } from '@mui/styles';
-import { Theme } from '@mui/material/styles';
 import CheckIcon from '@mui/icons-material/Check';
 import {
   GenericCardContent,
@@ -23,70 +21,14 @@ import MonsterCardForm from '../cards/newCardForms/MonsterCardForm';
 import { DEFAULT_TABS } from '../../constants';
 import CardTypeSelect from '../cards/cardFields/CardTypeSelect';
 import CardColumnSelect from '../cards/cardFields/CardColumnSelect';
-
-const useStyles = makeStyles<Theme>((theme) => ({
-  modal: {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: '50%',
-    height: '80%',
-    backgroundColor: '#ffffff',
-    border: 'none',
-    borderRadius: theme.spacing(1.5),
-    boxShadow: '24px',
-    padding: theme.spacing(2),
-    display: 'flex',
-    flexDirection: 'column',
-  },
-  form: {
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'space-between',
-    width: '100%',
-    overflowY: 'scroll',
-  },
-  content: {
-    display: 'flex',
-    flexDirection: 'column',
-    padding: theme.spacing(2),
-    overflowY: 'scroll',
-    fontSize: theme.spacing(6),
-    fontWeight: 400,
-    alignItems: 'flex-start',
-    justifyContent: 'space-between',
-  },
-  modalTitle: {
-    margin: theme.spacing(4),
-    paddingX: theme.spacing(2),
-    paddingTop: theme.spacing(1.5),
-  },
-  cancelButton: {
-    alignSelf: 'center',
-    justifyContent: 'center',
-    width: 'fit-content',
-    padding: theme.spacing(1),
-    marginLeft: 'auto',
-  },
-  saveButton: {
-    alignSelf: 'center',
-    justifyContent: 'center',
-    width: 'fit-content',
-    padding: theme.spacing(1),
-    marginLeft: 'auto',
-    '& svg': {
-      marginRight: theme.spacing(1),
-    },
-  },
-}));
+import CardBodyLayout from '../layout/CardBodyLayout';
+import RowLayout from '../layout/RowLayout';
 
 const NewCardDialog: React.FC<{
   isVisible: boolean;
   closeNewCardDialog: () => void;
   createCard: (cardData: CardData) => void;
 }> = ({ isVisible, createCard, closeNewCardDialog }) => {
-  const classes = useStyles();
   const id = uuidv4();
   const activeTab = useReadLocalStorage<string>('activeTab') ?? DEFAULT_TABS[0];
   const [content, setContent] = useState({} as GenericCardContent);
@@ -132,46 +74,104 @@ const NewCardDialog: React.FC<{
       aria-describedby="modal-description"
       data-testid="expanded-card"
     >
-      <Box className={classes.modal}>
+      <Box
+        sx={(theme) => {
+          return {
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            width: '50%',
+            height: '80%',
+            backgroundColor: '#ffffff',
+            border: 'none',
+            borderRadius: theme.spacing(1.5),
+            boxShadow: '24px',
+            padding: theme.spacing(2),
+            display: 'flex',
+            flexDirection: 'column',
+          };
+        }}
+      >
         <Typography
           id="modal-title"
-          sx={{ alignSelf: 'center' }}
-          className={classes.modalTitle}
           variant="h3"
           component="h3"
+          sx={(theme) => {
+            return {
+              margin: theme.spacing(4),
+              paddingX: theme.spacing(2),
+              paddingTop: theme.spacing(1.5),
+              alignSelf: 'center',
+            };
+          }}
         >
           Create Card
         </Typography>
-        <Box className={classes.content}>
-          <Box className={classes.form}>
+        <Box
+          sx={(theme) => {
+            return {
+              display: 'flex',
+              flexDirection: 'column',
+              padding: theme.spacing(2),
+              overflowY: 'scroll',
+              fontSize: theme.spacing(6),
+              fontWeight: 400,
+              alignItems: 'flex-start',
+              justifyContent: 'space-between',
+            };
+          }}
+        >
+          <CardBodyLayout
+            sxOverrides={{
+              justifyContent: 'space-between',
+              width: '100%',
+            }}
+          >
             <Box sx={{ display: 'flex', flexDirection: 'column', mb: 1 }}>
               <CardTypeSelect cardType={cardType} setCardType={setCardType} />
               <CardColumnSelect cardColumn={cardColumn} setCardColumn={setCardColumn} />
             </Box>
             <Box sx={{ display: 'flex', flexDirection: 'column', mb: 2 }}>{renderCardForm()}</Box>
-          </Box>
-          <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', width: '100%' }}>
+          </CardBodyLayout>
+          <RowLayout sxOverrides={{ justifyContent: 'space-between' }}>
             <Button
               variant="outlined"
-              className={classes.cancelButton}
               aria-label="cancel-button"
               data-testid="cancel-button"
               onClick={handleCancel}
+              sx={(theme) => {
+                return {
+                  alignSelf: 'center',
+                  justifyContent: 'center',
+                  width: 'fit-content',
+                  padding: theme.spacing(1),
+                  marginLeft: 'auto',
+                };
+              }}
             >
               Cancel
             </Button>
             <Button
               variant="contained"
               disabled={cardType === CardType.Rule && !content?.title}
-              className={classes.saveButton}
               aria-label="save-button"
               data-testid="save-button"
               onClick={handleSave}
+              sx={(theme) => {
+                return {
+                  alignSelf: 'center',
+                  justifyContent: 'center',
+                  width: 'fit-content',
+                  padding: theme.spacing(1),
+                  marginLeft: 'auto',
+                };
+              }}
             >
               <CheckIcon />
               Save
             </Button>
-          </Box>
+          </RowLayout>
         </Box>
       </Box>
     </Modal>
--- src/components/dialogs/VerificationDialog.tsx
+++ src/components/dialogs/VerificationDialog.tsx
@@ -1,48 +1,6 @@
 import React from 'react';
 import { Modal, Box, Button, Typography } from '@mui/material';
-import { makeStyles } from '@mui/styles';
 import CheckIcon from '@mui/icons-material/Check';
-import { Theme } from '@mui/material/styles';
-
-const useStyles = makeStyles<Theme, { confirmOnly: boolean }>((theme) => ({
-  modal: {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    backgroundColor: '#ffffff',
-    border: 'none',
-    borderRadius: theme.spacing(1.5),
-    boxShadow: '24px',
-    padding: theme.spacing(4),
-    display: 'flex',
-    flexDirection: 'column',
-    maxWidth: '30%',
-  },
-  body: {
-    display: 'flex',
-    flexDirection: 'column',
-    width: '100%',
-    height: '100%',
-    paddingBottom: theme.spacing(4),
-  },
-  buttonArea: {
-    width: '100%',
-    display: 'flex',
-    alignSelf: 'flex-end',
-    justifyContent: ({ confirmOnly }) => (confirmOnly ? 'center' : 'space-between'),
-    alignItems: 'center',
-  },
-  modalButton: {
-    alignSelf: 'center',
-    justifyContent: 'center',
-    width: theme.spacing(15),
-    marginLeft: 'auto',
-    '& svg': {
-      marginLeft: theme.spacing(1),
-    },
-  },
-}));
 
 interface VerificationDialogProps {
   dialogMessage: string;
@@ -59,7 +17,6 @@ const VerificationDialog: React.FC<VerificationDialogProps> = ({
   dialogOpen,
   confirmOnly = false,
 }) => {
-  const classes = useStyles({ confirmOnly: confirmOnly });
   return (
     <Modal
       open={dialogOpen}
@@ -68,28 +25,83 @@ const VerificationDialog: React.FC<VerificationDialogProps> = ({
       aria-describedby="modal-description"
       data-testid="expanded-card"
     >
-      <Box className={classes.modal}>
-        <Box className={classes.body}>
+      <Box
+        sx={(theme) => {
+          return {
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            backgroundColor: '#ffffff',
+            border: 'none',
+            borderRadius: theme.spacing(1.5),
+            boxShadow: '24px',
+            padding: theme.spacing(4),
+            display: 'flex',
+            flexDirection: 'column',
+            maxWidth: '30%',
+          };
+        }}
+      >
+        <Box
+          sx={(theme) => {
+            return {
+              display: 'flex',
+              flexDirection: 'column',
+              width: '100%',
+              height: '100%',
+              paddingBottom: theme.spacing(4),
+            };
+          }}
+        >
           <Typography variant="h6">{dialogMessage}</Typography>
         </Box>
-        <Box className={classes.buttonArea}>
+        <Box
+          sx={{
+            width: '100%',
+            display: 'flex',
+            alignSelf: 'flex-end',
+            justifyContent: () => (confirmOnly ? 'center' : 'space-between'),
+            alignItems: 'center',
+          }}
+        >
           {!confirmOnly && (
             <Button
               variant="outlined"
-              className={classes.modalButton}
               aria-label="cancel-button"
               onClick={handleCancel}
               data-testid="cancel-button"
+              sx={(theme) => {
+                return {
+                  alignSelf: 'center',
+                  justifyContent: 'center',
+                  width: theme.spacing(15),
+                  marginLeft: 'auto',
+                  '& svg': {
+                    marginLeft: theme.spacing(1),
+                  },
+                };
+              }}
             >
               Cancel
             </Button>
           )}
           <Button
             variant="contained"
-            className={classes.modalButton}
             aria-label="edit-confirm-button"
             onClick={handleConfirm}
             data-testid="confirm-button"
+            sx={(theme) => {
+              return {
+                alignSelf: 'center',
+                justifyContent: 'center',
+                width: theme.spacing(15),
+                marginLeft: 'auto',
+                '& svg': {
+                  marginLeft: theme.spacing(1),
+                },
+              };
+            }}
           >
             Confirm
             <CheckIcon />
