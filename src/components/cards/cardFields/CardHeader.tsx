bc7806ae3e4b0167724ef9fa6892130230d0bc4e
John Eckert <johnteckert@gmail.com>
2023-12-21 13:26:32 -0500

    refactor

--- src/components/cards/cardFields/CardHeader.tsx
+++ src/components/cards/cardFields/CardHeader.tsx
@@ -1,34 +1,23 @@
 import React from 'react';
-import Box from '@mui/material/Box';
+import RowLayout from '../../layout/RowLayout';
 import { Typography, IconButton } from '@mui/material';
 import EditIcon from '@mui/icons-material/Edit';
-import { useTheme } from '@mui/material/styles';
 
 const CardHeader: React.FC<{ title: string; handleEdit: () => void }> = ({ title, handleEdit }) => {
-  const theme = useTheme();
   return (
-    <Box
-      id="card-header"
-      sx={{
-        display: 'flex',
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'left',
-        width: '100%',
-      }}
-    >
+    <RowLayout id="card-header" sxOverrides={{ padding: 0 }}>
       <Typography id="card-title" variant="cardHeader" data-testid="title-view">
         {title}
       </Typography>
       <IconButton
-        sx={{ width: 'fit-content', padding: theme.spacing(1), marginLeft: 'auto' }}
+        sx={{ width: 'fit-content', marginLeft: 'auto' }}
         aria-label="edit-save-button"
         data-testid="edit-button"
         onClick={handleEdit}
       >
         <EditIcon />
       </IconButton>
-    </Box>
+    </RowLayout>
   );
 };
 
