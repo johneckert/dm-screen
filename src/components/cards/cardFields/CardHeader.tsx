5ddee610847a858a0aa40d941c273d11f1b1745e
John Eckert <johnteckert@gmail.com>
2023-12-21 12:31:41 -0500

    add typography variant

--- src/components/cards/cardFields/CardHeader.tsx
+++ src/components/cards/cardFields/CardHeader.tsx
@@ -2,55 +2,26 @@ import React from 'react';
 import Box from '@mui/material/Box';
 import { Typography, IconButton } from '@mui/material';
 import EditIcon from '@mui/icons-material/Edit';
-import { CardType } from '../../../interfaces';
+import { useTheme } from '@mui/material/styles';
 
-const CardHeader: React.FC<{ title: string; handleEdit: () => void; cardType?: CardType | null }> = ({
-  title,
-  handleEdit,
-  cardType,
-}) => {
+const CardHeader: React.FC<{ title: string; handleEdit: () => void }> = ({ title, handleEdit }) => {
+  const theme = useTheme();
   return (
     <Box
       id="card-header"
-      sx={(theme) => {
-        return {
-          display: 'flex',
-          flexDirection: 'row',
-          alignItems: 'center',
-          justifyContent: 'left',
-          marginBottom: theme.spacing(2),
-          width: '100%',
-        };
+      sx={{
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'left',
+        width: '100%',
       }}
     >
-      <Typography id="title-label" variant="h6" component="h6" data-testid="type-view">
-        {(cardType && cardType === CardType.Monster) || (cardType && cardType === CardType.Player)
-          ? 'Name: '
-          : 'Title: '}
-      </Typography>
-      <Typography
-        id="card-title"
-        sx={(theme) => {
-          return {
-            margin: theme.spacing(4),
-            paddingLeft: theme.spacing(2),
-            paddingTop: theme.spacing(0),
-          };
-        }}
-        variant="h6"
-        component="h6"
-        data-testid="title-view"
-      >
+      <Typography id="card-title" variant="cardHeader" data-testid="title-view">
         {title}
       </Typography>
       <IconButton
-        sx={(theme) => {
-          return {
-            width: 'fit-content',
-            padding: theme.spacing(1),
-            marginLeft: 'auto',
-          };
-        }}
+        sx={{ width: 'fit-content', padding: theme.spacing(1), marginLeft: 'auto' }}
         aria-label="edit-save-button"
         data-testid="edit-button"
         onClick={handleEdit}
