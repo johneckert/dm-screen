e5c6777d77b1f7f11e0bc8fbad7bfbb22c392cef
John Eckert <johnteckert@gmail.com>
2023-12-21 12:35:01 -0500

    refactor style decleration

--- src/components/layout/RowLayout.tsx
+++ src/components/layout/RowLayout.tsx
@@ -1,23 +1,22 @@
 import React, { ReactNode } from 'react';
 import { Box } from '@mui/material';
-import { SxProps, Theme } from '@mui/material/styles';
+import { SxProps, Theme, useTheme } from '@mui/material/styles';
 
 const RowLayout: React.FC<{ id?: string; sxOverrides?: SxProps<Theme>; children: ReactNode }> = ({
   id,
   sxOverrides = {},
   children,
 }) => {
+  const theme = useTheme();
+  const styles: SxProps<Theme> = {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '100%',
+    padding: theme.spacing(3),
+  };
   return (
-    <Box
-      id={id}
-      sx={{
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-        width: '100%',
-        ...sxOverrides,
-      }}
-    >
+    <Box id={id} sx={{ ...styles, ...sxOverrides }}>
       {children}
     </Box>
   );
